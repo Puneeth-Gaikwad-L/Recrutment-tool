630a61ede89bd155d9a977199e5326d2b2a0df3c
Puneeth Gaikwad <puneethgaikwadl@gmail.com>
2024-01-31 17:14:03 +0530

    initial commit

---
+++ recrutment-tool/src/main/java/com/example/recrutmenttool/RecrutmentToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.recrutmenttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutmentToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutmentToolApplication.class, args);
+	}
+
+}
