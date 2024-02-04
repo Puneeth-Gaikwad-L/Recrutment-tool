3846ed025a3802271bad1982a730ffdb742e0bd8
ANKIT CHANDRA <str.ankit99@gmail.com>
2024-02-04 12:42:25 +0530

    version1 upload

--- recrutment-tool/src/main/java/com/example/recrutmenttool/models/Documents.java
+++ recrutment-tool/src/main/java/com/example/recrutmenttool/models/Documents.java
@@ -1,4 +1,34 @@
 package com.example.recrutmenttool.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Documents {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer documentId;
+
+    private String fileName;
+
+    private Date uploadDate;
+
+    private String userInformation;
+
+    private String documentUrl;
+
 }
