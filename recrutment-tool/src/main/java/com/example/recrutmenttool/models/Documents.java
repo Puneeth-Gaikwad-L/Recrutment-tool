c744852200ca59adef6a4c2ccae3c43dfa90b09a
ANKIT CHANDRA <str.ankit99@gmail.com>
2024-02-12 19:28:06 +0530

    Add files via upload

---
+++ recrutment-tool/src/main/java/com/example/recrutmenttool/models/Documents.java
@@ -0,0 +1,44 @@
+package com.example.recrutmenttool.models;
+
+
+import com.example.recrutmenttool.Enum.DocumentStatus;
+import com.example.recrutmenttool.Enum.FileType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Documents {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer documentId;
+
+    private String userId;
+
+    @Enumerated (value = EnumType.STRING)
+    private FileType fileType;
+
+    private Integer fileSize;
+
+    private Date uploadDate;
+
+    @Enumerated (value = EnumType.STRING)
+    private DocumentStatus status;
+
+    private String documentUrl;
+
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+}
